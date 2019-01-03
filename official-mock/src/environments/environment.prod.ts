10a5489999de3bf9c2f8c05b517232c7a4d4fe3f
Emmanuel Shutsha <Emmanuel.Shutsha@nology.io>
2019-01-03 09:11:58 +0000

    added firebase link

--- official-mock/src/environments/environment.prod.ts
+++ official-mock/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDamf4bjSHmaHhARYL2w79DpZL5ZkgYU8M",
+    authDomain: "mocking-official-e59c7.firebaseapp.com",
+    databaseURL: "https://mocking-official-e59c7.firebaseio.com",
+    projectId: "mocking-official-e59c7",
+    storageBucket: "mocking-official-e59c7.appspot.com",
+    messagingSenderId: "925001145426"
+  },
 };
--- official-mock/src/environments/environment.ts
+++ official-mock/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDamf4bjSHmaHhARYL2w79DpZL5ZkgYU8M",
+    authDomain: "mocking-official-e59c7.firebaseapp.com",
+    databaseURL: "https://mocking-official-e59c7.firebaseio.com",
+    projectId: "mocking-official-e59c7",
+    storageBucket: "mocking-official-e59c7.appspot.com",
+    messagingSenderId: "925001145426"
+  },
 };
 
 /*
