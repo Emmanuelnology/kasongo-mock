3f7a2f12bc83ac77830a364a07c46a155a38e384
Emmanuel Shutsha <Emmanuel.Shutsha@nology.io>
2019-01-03 09:49:08 +0000

    added routing and home component

--- official-mock/src/app/app.module.ts
+++ official-mock/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -10,19 +11,29 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'kasongo-mock'),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
