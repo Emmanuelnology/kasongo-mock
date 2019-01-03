c1516892f30aea13924ebba6ef19e3a7c3d8ad03
Emmanuel Shutsha <Emmanuel.Shutsha@nology.io>
2019-01-03 13:02:32 +0000

    added user authentification

--- official-mock/src/app/services/auth.service.ts
+++ official-mock/src/app/services/auth.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+export interface User {
+  uid: string;
+  email: string;
+  photoURL: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user: Observable<User>;
+
   register(email: string, password: string) {
     return this.afAuth.auth
       .createUserWithEmailAndPassword(email, password)
@@ -15,5 +26,5 @@ export class AuthService {
       });
   }
 
-  constructor(private afAuth: AngularFireAuth, private route: Router ) { }
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore, private route: Router ) { }
 }
